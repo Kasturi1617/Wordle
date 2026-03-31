2b1f2b017af212913c323ca9e918e50f7b46f2fe
Kasturi Sanyal <kasturisanyal3@gmail.com>
2026-04-01 02:35:09 +0530

    Backspace and Enter key handling added

--- src/Grid.tsx
+++ src/Grid.tsx
@@ -1,16 +1,23 @@
 import styles from './Grid.module.css';
 import { useEffect, useState } from 'react';
+import WORDS from './WORDS.ts';
 
 function Grid() {
     const MAX_ATTEMPTS = 6;
     const WORD_LENGTH = 5;
+    const [targetWord, setTargetWord] = useState('');
+
+    useEffect(() => {
+        const randomIndex = Math.floor(Math.random() * WORDS.length);
+        setTargetWord(WORDS[randomIndex].toUpperCase());
+    }, []);
 
     const initialGrid = Array.from({ length: MAX_ATTEMPTS }, () => Array(WORD_LENGTH).fill(''));
 
     const [gameState, setGameState] = useState({
         grid: initialGrid,
         currentRow: 0,
-        currentCol: 0,
+        currentCol: 0
     });
 
     const handleKeyDown = (event: KeyboardEvent) => {
@@ -18,14 +25,53 @@ function Grid() {
             setGameState(prev => {
                 const { grid, currentRow, currentCol } = prev;
                 const newGrid = grid.map(row => [...row]);
-                newGrid[currentRow][currentCol] = event.key.toUpperCase();
+                if (currentCol < WORD_LENGTH) {
+                    newGrid[currentRow][currentCol] = event.key.toUpperCase();
+                }
 
                 let newCol = currentCol + 1;
-                let newRow = currentRow;
-                if (newCol >= WORD_LENGTH) {
-                    newCol = 0;
-                    newRow = currentRow + 1;
+                if (newCol > WORD_LENGTH) {
+                    newCol = WORD_LENGTH;
                 }
+                let newRow = currentRow;
+
+                return {
+                    grid: newGrid,
+                    currentRow: newRow,
+                    currentCol: newCol,
+                };
+            });
+        }
+        else if (event.key === 'Enter') {
+
+            setGameState(prev => {
+                const { grid, currentRow, currentCol } = prev;
+                if (currentCol < WORD_LENGTH) {
+                    return prev;
+                }
+                const newGrid = grid.map(row => [...row]);
+
+                let newCol = 0;
+                let newRow = currentRow + 1;
+
+                return {
+                    grid: newGrid,
+                    currentRow: newRow,
+                    currentCol: newCol,
+                };
+            });
+        }
+        else if (event.key === 'Backspace') {
+            setGameState(prev => {
+                const { grid, currentRow, currentCol } = prev;
+                const newGrid = grid.map(row => [...row]);
+                let newCol = currentCol - 1;
+                if (newCol < 0) {
+                    newCol = 0;
+                }
+                newGrid[currentRow][newCol] = '';
+
+                let newRow = currentRow;
 
                 return {
                     grid: newGrid,
